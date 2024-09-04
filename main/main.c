e62d0060d3e2127dcfa576aeefd8195ba88602a0
davdd1 <stenmandavid03@gmail.com>
2024-09-04 09:55:23 +0200

    Update gitignore

---
+++ main/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#include "nvs.h"
+#include "nvs_flash.h"
+
+#include "esp_bt.h"
+#include "esp_bt_main.h"
+#include "esp_gap_ble_api.h"
+#include "esp_gatts_api.h"
+#include "esp_gattc_api.h"
+#include "esp_bt_defs.h"
+#include "esp_bt_device.h"
+
+
+void app_main(void)
+{
+    esp_err_t ret = nvs_flash_init();
+    // Intialize NVS 
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) 
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    // Initialize Bluetooth
+    esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
+    esp_bt_controller_init(&bt_cfg);
+    esp_bt_controller_enable(ESP_BT_MODE_BLE);
+   
+    esp_bluedroid_init();
+    esp_bluedroid_enable();
+    esp_bt_dev_set_device_name("ESP32_BLE");
+
+    
+
+}
